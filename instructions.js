84cbce5bfbacbf34d069faf91c09e3d5394c8b56
wyattlaberge64 <42870908+wyattlaberge64@users.noreply.github.com>
2018-11-02 09:55:50 -0400

    Create instructions.js

---
+++ instructions.js
@@ -0,0 +1,15 @@
+/* MULTIPLICATION QUIZ KATA */
+
+/* INSTRUCTIONS for PART 1 */
+
+// 1. Print 2 Random Integers, X & Y, each between 3 and 9, as a soluble equation:  X * Y = ??
+// 2. Receive manual input for solution, say "Correct!" if match, "Incorrect!" if not.
+// 3. Embed equation in While loop that continues until entry matches solution, or = "stop".
+// 4. Embed all code in a For loop and run it 10 times.
+
+/* INSTRUCTIONS for PART 2: REFACTOR INTO FUNCTIONS */
+
+// 5. Move code parts 1-3 into a function called multiPractice, and call multiPractice within the For loop.
+// 6. Count how many not-right-first-answers and return that count from multiPractice.
+// 7. At "stop" or loop end, report # answers wrong.  If no wrong answers, say "Charlie, you've won!"
+// 8. Extra Credit: instead of 10 times, run loop to value of questions variable entered manually.
